90f29fbd1ff34e876f3eea83c85fc0fcb7e95d17
Bossé <BOSSEE@dfo-mpo.ca>
2018-07-31 08:49:21 -0400

    rest app exe de base

---
+++ SERVER/src/restApp/restApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Net;
+
+namespace restApp
+{
+
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract, WebInvoke(UriTemplate = "/sum", Method = "POST", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        double sum(double x, double y);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public double sum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9011/Calculator";
+            WebServiceHost myHost = new WebServiceHost(typeof(Calculator), new Uri(baseAddress));
+            Console.WriteLine("starting service...");
+            myHost.Open();
+            Console.WriteLine("press any key to close the service...");
+            Console.ReadLine();
+            myHost.Close();
+        }
+    }
+}
