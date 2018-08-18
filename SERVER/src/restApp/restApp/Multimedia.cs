4377457c901d9752a61cad9fdf559d5f0b19319e
Émile Bossé <emile.bosse@cidco.ca>
2018-08-18 11:45:19 -0400

    restApp fonctionnal

--- SERVER/src/restApp/restApp/Multimedia.cs
+++ SERVER/src/restApp/restApp/Multimedia.cs
@@ -29,7 +29,7 @@ namespace restApp
         [OperationContract, WebInvoke(UriTemplate = "/music?name={name}", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
         string getMusic(string name);
 
-        [OperationContract, WebInvoke(UriTemplate = "/musics", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract, WebInvoke(UriTemplate = "/music", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
         string getAllMusic();
     }
 
@@ -41,17 +41,29 @@ namespace restApp
 
         public string getPhoto(string name)
         {
-            return utils.getJson(createPhotoEntity(pathPhoto + "\\" + name));
+            PhotoEntity photo = new PhotoEntity();
+            photo = (PhotoEntity)createEntity(pathPhoto + "\\" + name, photo);
+            if(photo != null)
+                return utils.getJson(photo);
+            return "{No photo with this name.}";
         }
 
         public string getVideo(string name)
         {
-            return utils.getJson(createVideoEntity(pathVideo + "\\" + name));
+            VideoEntity video = new VideoEntity();
+            video = (VideoEntity)createEntity(pathVideo + "\\" + name, video);
+            if(video != null)
+                return utils.getJson(video);
+            return "{No video with this name.}";
         }
 
         public string getMusic(string name)
         {
-            return utils.getJson(createMusicEntity(pathMusic + "\\" + name));
+            MusicEntity music = new MusicEntity();
+            music = (MusicEntity)createEntity(pathMusic + "\\" + name, music);
+            if (music != null)
+                return utils.getJson(music);
+            return "{No music with this name.}";
         }
 
         public string getAllPhoto()
@@ -60,7 +72,8 @@ namespace restApp
             string[] dirs = Directory.GetFiles(pathPhoto, "*");
             foreach (string file in dirs)
             {
-                PhotoEntity p = createPhotoEntity(file);
+                PhotoEntity p = new PhotoEntity();
+                p = (PhotoEntity)createEntity(file, p);
                 if (p != null)
                 {
                     photos.Add(p);
@@ -71,14 +84,15 @@ namespace restApp
 
         public string getAllVideo()
         {
-            List<VideoEntity> videos = new List<VideoEntity>();
+            List<VideoEntityView> videos = new List<VideoEntityView>();
             string[] dirs = Directory.GetFiles(pathVideo, "*");
             foreach (string file in dirs)
             {
-                VideoEntity p = createVideoEntity(file);
+                VideoEntity p = new VideoEntity();
+                p = (VideoEntity)createEntity(file, p);
                 if (p != null)
                 {
-                    videos.Add(p);
+                    videos.Add(new VideoEntityView(p));
                 }
             }
             return utils.getJson(videos);
@@ -86,42 +100,46 @@ namespace restApp
 
         public string getAllMusic()
         {
-            List<MusicEntity> videos = new List<MusicEntity>();
+            List<MusicEntityView> videos = new List<MusicEntityView>();
             string[] dirs = Directory.GetFiles(pathMusic, "*");
             foreach (string file in dirs)
             {
-                MusicEntity p = createMusicEntity(file);
+                MusicEntity p = new MusicEntity();
+                p = (MusicEntity)createEntity(file, p);
                 if (p != null)
                 {
-                    videos.Add(p);
+                    videos.Add(new MusicEntityView(p));
                 }
             }
             return utils.getJson(videos);
         }
 
-        private MusicEntity createMusicEntity(string path)
+        private Entity createEntity(string path, Entity type)
         {
-            return new MusicEntity(path);
-        }
-
-        private PhotoEntity createPhotoEntity(string path)
-        {
-            return new PhotoEntity(path);
-        }
-
-        private VideoEntity createVideoEntity(string path)
-        {
-            return new VideoEntity(path);
+            if (utils.isValid(path.Split('.')[path.Split('.').Length - 1], type.getValidType()))
+            {
+                type.setEntity(path);
+                return type;
+            }
+            return null;
         }
     }
- 
-    class PhotoEntity
+
+    interface Entity
+    {
+        string[] getValidType();
+        void setEntity(string path);
+    }
+
+    class PhotoEntity : Entity
     {
         public byte[] img { get; set; }
         public string name { get; set; }
         public string path { get; set; }
         public string type { get; set; }
+        private string[] validType = { "png", "PNG", "JPEG", "jpeg", "jpg", "JPG" };
 
+        public PhotoEntity() { }
         public PhotoEntity(string path)
         {
             this.path = path;
@@ -129,40 +147,124 @@ namespace restApp
             this.type = path.Split('.')[path.Split('.').Length - 1];
             this.img = File.ReadAllBytes(path);
         }
+
+        public void setEntity(string path)
+        {
+            Console.WriteLine("in the Pictures one");
+            this.path = path;
+            this.name = path.Split('\\')[path.Split('\\').Length - 1];
+            this.type = path.Split('.')[path.Split('.').Length - 1];
+            this.img = File.ReadAllBytes(path);
+        }
+
+        public string[] getValidType()
+        {
+            return this.validType;
+        }
     }
 
-    class VideoEntity
+    class VideoEntity : Entity
     {
         public byte[] vid { get; set; }
         public string name { get; set; }
         public string path { get; set; }
         public string type { get; set; }
+        private string[] validType = { "mp4", "MP4", "wav", "WAV" };
+
+        public VideoEntity() { }
         public VideoEntity(string path)
         {
             this.path = path;
             this.name = path.Split('\\')[path.Split('\\').Length - 1];
-            this.type = path.Split('\\')[path.Split('\\').Length - 1];
+            this.type = path.Split('.')[path.Split('.').Length - 1];
             this.vid = File.ReadAllBytes(path);
+        }
+
+        public void setEntity(string path)
+        {
+            Console.WriteLine("in the Video one");
+            this.path = path;
+            this.name = path.Split('\\')[path.Split('\\').Length - 1];
+            this.type = path.Split('.')[path.Split('.').Length - 1];
+            this.vid = File.ReadAllBytes(path);
+        }
+
+        public string[] getValidType()
+        {
+            return this.validType;
         }
     }
 
-    class MusicEntity
+    class VideoEntityView
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public string type { get; set; }
+        public VideoEntityView(VideoEntity entity)
+        {
+            name = entity.name;
+            path = entity.path;
+            type = entity.type;
+        }
+    }
+
+    class MusicEntity : Entity
     {
         public byte[] song { get; set; }
         public string name { get; set; }
         public string path { get; set; }
         public string type { get; set; }
+        private string[] validType = { "mp3", "MP3" };
+
+        public MusicEntity() { }
         public MusicEntity(string path)
         {
             this.path = path;
             this.name = path.Split('\\')[path.Split('\\').Length - 1];
-            this.type = path.Split('\\')[path.Split('\\').Length - 1];
+            this.type = path.Split('.')[path.Split('.').Length - 1];
             this.song = File.ReadAllBytes(path);
+        }
+
+        public void setEntity(string path)
+        {
+            Console.WriteLine("in the Music one");
+            this.path = path;
+            this.name = path.Split('\\')[path.Split('\\').Length - 1];
+            this.type = path.Split('.')[path.Split('.').Length - 1];
+            this.song = File.ReadAllBytes(path);
+        }
+
+        public string[] getValidType()
+        {
+            return this.validType;
+        }
+    }
+
+    class MusicEntityView
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public string type { get; set; }
+        public MusicEntityView(MusicEntity entity)
+        {
+            name = entity.name;
+            path = entity.path;
+            type = entity.type;
         }
     }
 
     class utils
     {
+        public static bool isValid(string type, string[] validType)
+        {
+            foreach(string s in validType)
+            {
+                if (s == type)
+                    return true;
+            }
+            return false;
+        }
+
         public static string getJson(Object obj)
         {
             return new JavaScriptSerializer().Serialize(obj);
